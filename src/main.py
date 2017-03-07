175e84424f6aec0a8be0f71ffdf09931ef4431b9
Grant Weaver <gtweaver4@gmail.com>
2017-03-07 00:01:12 -0500

    created the staff using the line method

--- src/main.py
+++ src/main.py
@@ -8,40 +8,46 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.units import inch
 from PIL import Image
 
-#11.5 top of paper very top of paper for draw string
-
-
-'''
-Tried to get the jpg image of the sheet music to be drawn and it doesnt work
-def draw_bass_cleff(c):
-	bass_cleff = Image.open("../img/BassCleff/CommonTimeCMajorBassCleff")
-	bass_cleff.load()
-	c.drawImage(bass_cleff, 0, 12*inch, width = None, height = None, mask = None)
-'''
 
 
 
+
+#returns the newly created canvas (8x11.5)
 def create_canvas():
-	'''
-	c.translate(inch,inch)
-	c.setFont("Helvetica", 14)
-	c.setStrokeColorRGB(0.2,0.5,0.3)
-	c.setFillColorRGB(1,0,1)
-	c.line(0,10.5*inch,0,1.7*inch)
-	c.line(0,10*inch,1*inch,0)
-	'''
 	c = canvas.Canvas("test.pdf")
 	c.setTitle("Sheet Music")
 	c.setAuthor("Random Sheet Music Generator")
 	c.drawString(3*inch, 11*inch, "Created by Random Sight Reader")
-
 	return c
+
+
+#draws the staff ONLY
+#measure lines will be drawn seperately based on key sig
+def draw_bars(c):
+	y = 10.5
+	x = .5
+	for i in range(0,10):
+		#using temp so there aren't as many draw line functions
+		temp = y 
+		for j in range(0,5):
+			c.line(.5*inch,y*inch, 7.5*inch, y*inch)
+
+			#draw side bars
+			if(j == 4):
+				c.line(.5*inch, temp*inch, .5*inch, y*inch)
+				c.line(7.5*inch, temp*inch, 7.5*inch, y*inch)
+
+			y = y - .15
+		y = y - .25
+		
+
+
 
 
 #creating the canvas to draw on
 c = create_canvas()
 
-
+draw_bars(c)
 
 
 
